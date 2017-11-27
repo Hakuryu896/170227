e963ce8329711e6461b303177c67c6dafbebcc44
Hakuryu896 <170227ff@yse-c.net>
2017-11-27 10:15:53 +0900

    Hello2
    Hello2表示

--- Hello2.java
+++ Hello2.java
@@ -1,5 +1,14 @@
-public class Hello2{
+import javax.swing.JFrame;
+public class Hello2 extends JFrame{
+	Hello2(String title){
+		setTitle(title);
+		setSize(300,300);
+		setLocation(300,300);
+		setDefaultCloseOperation)JFrame.EXIT_ON_CLOSE);
+}
 	public static void main(String[] args){
+		Hello2 h2 = new Hello2("Hello");
+		setVisible(true);
 		System.out.println("Hello");
 	}
 }
